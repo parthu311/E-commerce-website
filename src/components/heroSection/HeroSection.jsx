cbe6ff1c41a5362b9e4bceee9e7b04e5fdea5829
parthu311 <parthradadiya433@gmail.com>
2024-02-12 18:25:41 +0530

    done

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import bgimg from "../../assets/bg2.jpg"
 
 const HeroSection = () => {
   return (
     <div>
-        <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="HeroSection" />
-    </div>
+    <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" />
+</div>
   )
 }
 
